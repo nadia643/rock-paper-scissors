bc7d72d60e43aaf38d98c43706d7c17afdedff72
John Doe <nadia643@gmail.com>
2020-01-15 13:26:08 +0000

    css change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let computerScore=0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_div = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -15,21 +15,34 @@ function computerPlay() {
     const items = ['rock', 'paper', 'scissors'];
     return items[Math.floor(Math.random()*items.length)];
 }
+const smallUserWord = "user".fontsize(3).sub();
+const smallCompWord = "comp".fontsize(3).sub();
 
-function win() {
+function convertToCapital(userSelection) {
+    if (userSelection === "rock") return "Rock";
+    if (userSelection === "paper") return "Paper";
+    return "Scissors";
+}
+function convertToCapital(computerSelection) {
+    if (computerSelection === "rock") return "Rock";
+    if (computerSelection === "paper") return "Paper";
+    return "Scissors";
+}
+
+
+function win(userSelection, computerSelection) {
     userScore++
     userScore_span.innerHTML = userScore;
-    console.log("User wins");
-    console.log(userScore);
+    result_div.innerHTML = convertToCapital(userSelection) + `${smallUserWord}` +" beats " + convertToCapital(computerSelection) + `${smallCompWord}` + ". You win!"
 }
-function lose() {
+function lose(userSelection, computerSelection) {
     computerScore++
     computerScore_span.innerHTML = computerScore;
-    console.log("Computer wins boooo");
-    console.log(computerScore);
+    result_div.innerHTML = convertToCapital(computerSelection) + `${smallCompWord}` + " beats " + convertToCapital(userSelection) + `${smallUserWord}` + ". You lose!"
+
 }
 function draw() {
-    console.log("DRAWWWW");
+    result_div.innerHTML = "Same same. Try again!"
 }
 
 function game(userSelection) {
@@ -40,17 +53,17 @@ function game(userSelection) {
         case "rockscissors":
         case "paperrock":
         case "scissorspaper":
-            win();
+            win(userSelection, computerSelection);
             break;
         case "rockpaper":
         case "paperscissors":
         case "scissorsrock":
-            lose();
+            lose(userSelection, computerSelection);
             break;
         case "rockrock":
         case "paperpaper":
         case "scissorsscissors":
-            draw();
+            draw(userSelection, computerSelection);
             break;
     }
 }
